ed221931ff0e320287f1bd4d04682b09a5ee4820
Andrew Kim <akinhwan@gmail.com>
2022-02-28 20:13:25 -0500

    initial commit

--- src/ClockFace.js
+++ src/ClockFace.js
@@ -2,6 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const generateRandomTime = () => {
+    const hours = Math.floor(Math.random() * 12);
+    const minutes = Math.floor(Math.random() * 60);
+    const seconds = Math.floor(Math.random() * 60);
+    return `${hours}:${minutes}:${seconds}`;
+  };
+
   return (
     <div className="App">
       <header className="App-header">
